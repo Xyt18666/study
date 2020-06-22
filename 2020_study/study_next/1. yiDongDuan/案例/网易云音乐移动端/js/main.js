16d9b462a3d6b4c5a95387da3e2e2d7edf4969c3
xyt <xytEmail>
2020-06-23 00:41:21 +0800

    d

--- 2020_study/study_next/1. yiDongDuan/案例/网易云音乐移动端/js/main.js
+++ 2020_study/study_next/1. yiDongDuan/案例/网易云音乐移动端/js/main.js
@@ -9,36 +9,40 @@ $(function () {
   //     },
   //   });
 
-  let ban = $(".banList")[0];
-  let index = 3;
-  let lateX = null;
+  if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+    // 判断是否是什么设备
+    console.log("移动端");
 
-  tab(index);
+    let ban = $(".banList")[0];
+    let index = 3;
+    let lateX = null;
 
-  ban.ontouchstart = function (e) {
-    let startX = parseInt(e.touches[0].clientX);
-    lateX = parseInt(
-      $(".banList")
-        .css("transform")
-        .replace(/[^0-9\-.,]/g, "")
-        .split(",")[4]
-    );
+    tab(index);
 
-    $(ban).css({
-      transition: "0s",
-      transform: `translateX(${lateX}px)`,
-    });
-
-    ban.ontouchmove = function (e) {
-      let moveX = parseInt(e.touches[0].clientX); //取整为了去除小数点
+    ban.ontouchstart = function (e) {
+      let startX = parseInt(e.touches[0].clientX);
+      lateX = parseInt(
+        $(".banList")
+          .css("transform")
+          .replace(/[^0-9\-.,]/g, "")
+          .split(",")[4]
+      );
 
       $(ban).css({
-        transform: `translateX(${lateX - (startX - moveX)}px)`,
+        transition: "0s",
+        transform: `translateX(${lateX}px)`,
       });
 
-      //   console.log(moveX, startX, lateX);
+      ban.ontouchmove = function (e) {
+        let moveX = parseInt(e.touches[0].clientX); //取整为了去除小数点
 
-      /*
+        $(ban).css({
+          transform: `translateX(${lateX - (startX - moveX)}px)`,
+        });
+
+        //   console.log(moveX, startX, lateX);
+
+        /*
       lateX - (startX - moveX)  
       按下时的 transformlate X  - (按下时的 x坐标 - 移动的 X 坐标)
     
@@ -46,87 +50,91 @@ $(function () {
       按下时的 x坐标 - 移动的 X 坐标 的结果 无论是正还是负，都正确显示
       
       */
-    };
+      };
 
-    ban.ontouchend = function (e) {
-      let endX = parseInt(e.changedTouches[0].clientX);
+      ban.ontouchend = function (e) {
+        let endX = parseInt(e.changedTouches[0].clientX);
 
-      if (Math.abs(endX - startX) < parseInt($(".ban").width()) / 3) {
-        tab(index);
-      } else {
-        if (endX - startX > 0) {
-          // 向右滑动
-          index--;
-
-          if (lateX > -parseInt($(".ban").width())) {
-            $(ban).css({
-              transition: "0s",
-              transform: `translateX(${
-                -parseInt($(".ban").width()) * 6 + (endX - startX)
-              }px)`,
-            });
-            index = 5;
-          }
+        if (Math.abs(endX - startX) < parseInt($(".ban").width()) / 3) {
           tab(index);
         } else {
-          // 向左滑动
-          index++;
-          if (lateX < -parseInt($(".ban").width()) * 5) {
-            $(ban).css({
-              transition: "0s",
-              transform: `translateX(${0 + (endX - startX)}px)`,
-            });
-            index = 1;
-          }
-          tab(index);
-        }
-      }
-    };
-  };
+          if (endX - startX > 0) {
+            // 向右滑动
+            index--;
 
-  function tab(index) {
-    $(ban).css({
-      transition: "0.5s",
-      transform: `translateX(${-index * parseInt($(".ban").width())}px)`,
+            if (lateX > -parseInt($(".ban").width())) {
+              $(ban).css({
+                transition: "0s",
+                transform: `translateX(${
+                  -parseInt($(".ban").width()) * 6 + (endX - startX)
+                }px)`,
+              });
+              index = 5;
+            }
+            tab(index);
+          } else {
+            // 向左滑动
+            index++;
+            if (lateX < -parseInt($(".ban").width()) * 5) {
+              $(ban).css({
+                transition: "0s",
+                transform: `translateX(${0 + (endX - startX)}px)`,
+              });
+              index = 1;
+            }
+            tab(index);
+          }
+        }
+      };
+    };
+
+    function tab(index) {
+      $(ban).css({
+        transition: "0.5s",
+        transform: `translateX(${-index * parseInt($(".ban").width())}px)`,
+      });
+
+      $(".banD li")
+        .eq(index - 1)
+        .css({
+          background: "pink",
+        })
+        .siblings()
+        .css({
+          background: "rgb(44, 195, 241)",
+        });
+    }
+
+    $(".r").click(() => {
+      index++;
+      console.log(index);
+      if (index > 5) {
+        $(ban).css({
+          transition: "0s",
+          transform: `translateX(0px)`,
+        });
+        index = 1;
+      }
+      tab(index);
+    });
+    $(".l").click(() => {
+      index--;
+      console.log(index);
+      if (index < 1) {
+        $(ban).css({
+          transition: "0s",
+          transform: `translateX(${-parseInt($(".ban").width()) * 6}px)`,
+        });
+        index = 5;
+      }
+      tab(index);
     });
 
-    $(".banD li")
-      .eq(index - 1)
-      .css({
-        background: "pink",
-      })
-      .siblings()
-      .css({
-        background: "rgb(44, 195, 241)",
-      });
+    var dsq = setInterval(() => {
+      $(".r").click();
+    }, 2000);
+  } else {
+    console.log("PC端");
+    $("body").html("pc端");
   }
-
-  $(".r").click(() => {
-    index++;
-    console.log(index);
-    if (index > 5) {
-      $(ban).css({
-        transition: "0s",
-        transform: `translateX(0px)`,
-      });
-      index = 1;
-    }
-    tab(index);
-  });
-  $(".l").click(() => {
-    index--;
-    console.log(index);
-    if (index < 1) {
-      $(ban).css({
-        transition: "0s",
-        transform: `translateX(${-parseInt($(".ban").width()) * 6}px)`,
-      });
-      index = 5;
-    }
-    tab(index);
-  });
-
-  var dsq = setInterval(() => {
-    $(".r").click();
-  }, 2000);
 });
