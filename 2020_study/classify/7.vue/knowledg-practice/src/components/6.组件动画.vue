7203cbe7aeff5a490f3f13b88a748c5d49ac557f
xyt <xytEmail>
2020-10-19 16:35:58 +0800

    个

---
+++ 2020_study/classify/7.vue/knowledg-practice/src/components/6.组件动画.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+transition(name="box")
+    .content(
+        v-show="show"
+    ) 我是内容   
+
+</template>
+
+<script>
+export default {
+    props: ["show"],
+};
+</script>
+
+<style lang="sass" scoped>
+
+.box-enter-active
+    animation: boxInter 1s
+
+.box-leave-active
+    animation: boxInter 1s reverse
+
+@keyframes boxInter
+    0%
+        top: -100%
+    100%
+        top: 0
+
+.content
+    width: 100%
+    height: 50px
+    position: fixed
+    top: 0
+    left: 0
+    background: #0f0
+</style>
--- 2020_study/classify/7.vue/knowledg-practice/src/views/Home.vue
+++ 2020_study/classify/7.vue/knowledg-practice/src/views/Home.vue
@@ -4,6 +4,11 @@
   bao-dan
   san
   dong
+  //- transition(name="qq")
+  ZuJianDong(
+    :show="show"
+  )
+  p(@click="show=!show") 修改 show
 </template>
 
 <script>
@@ -11,15 +16,27 @@ import HelloWorld from "@/components/2.class和style绑定.vue";
 import BaoDan from "@/components/3.表单绑定.vue";
 import San from "@/components/4.属性计算和监听器.vue";
 import Dong from "@/components/6.vue动画.vue";
-
+import ZuJianDong from "@/components/6.组件动画.vue";
 
 export default {
+    data() {
+        return {
+            show: false,
+        };
+    },
     name: "Home",
     components: {
         HelloWorld,
         BaoDan,
         San,
-        Dong
+        Dong,
+        ZuJianDong,
     },
 };
 </script>
+<style lang="sass" scoped>
+// .qq-enter,.qq-leave-to
+//     top: -100%
+// .qq-enter-active,.qq-leave-active
+//     transition: 1s
+</style>
