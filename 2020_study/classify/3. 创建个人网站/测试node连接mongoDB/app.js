2907b5f2ded6d9a4593b7e54cba36bf6186e8cc2
xyt <xytEmail>
2020-10-20 11:26:06 +0800

    ok

--- 2020_study/classify/3. 创建个人网站/测试node连接mongoDB/app.js
+++ 2020_study/classify/3. 创建个人网站/测试node连接mongoDB/app.js
@@ -1,27 +1,35 @@
-let http = require("http")
-let server = http.createServer()
-var MongoClient = require('mongodb').MongoClient;
+let http = require("http");
+//引入node.js中的 http模块 ，用于搭建简易 服务器
+
+var MongoClient = require("mongodb").MongoClient;
+// 引入mongodb ，创建连接
 var url = "mongodb://localhost:27017/test";
-server.on('request',function(req,ress){
-    if(req.url=='/'){
-        MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+// 数据库的地址
+
+http.createServer(function (req, ress) {
+    //创建简易 服务器
+    console.log(req.httpVersion); //输出相关参数
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    console.log(req.trailers);
+    console.log(req.complete);
+    if (req.url == "/") {
+        MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+            // 连接数据库
             if (err) throw err;
             var dbase = db.db("test");
-            dbase.collection('test'). find({}).toArray(function(err,res){
-                if(!err){
-                    ress.end(JSON.stringify(res))
-                    db.close();
-                }
-            })
-            
-          });
+            dbase
+                .collection("test")
+                .find({}) //找到所有数据，转为数组
+                .toArray(function (err, res) {
+                    if (!err) {
+                        ress.end(JSON.stringify(res));
+                        db.close();
+                        // 关闭连接
+                    }
+                });
+        });
     }
-})
-server.listen(3000,function(err){
-    if(!err){
-        console.log("打开localhost:3000")
-    }
-})
-
- 
-
+}).listen(8000);
+console.log("打开 http://localhost:8000/"); //先执行这个  再执行function中的
