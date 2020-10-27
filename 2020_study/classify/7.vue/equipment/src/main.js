e67e0b45279cbd7986e89982ad8222e9f9857fae
xyt <xytEmail>
2020-10-27 16:55:19 +0800

    s

--- 2020_study/classify/7.vue/equipment/src/main.js
+++ 2020_study/classify/7.vue/equipment/src/main.js
@@ -5,7 +5,9 @@ import App from "./App";
 import router from "./router";
 import store from "./store";
 
+import axios from "axios";
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 /* eslint-disable no-new */
 new Vue({
--- 2020_study/classify/7.vue/equipment/src/views/Login.vue
+++ 2020_study/classify/7.vue/equipment/src/views/Login.vue
@@ -20,34 +20,125 @@
                 .sms-from(v-show="type=='sms'")
                     label
                         input(
-                            placeholder='电话'
+                            placeholder='13800138001'
+                            v-model="phones"
                         )
                     .con
                         label
                             input(
-                                placeholder='验证码'
+                                placeholder='654321'
+                                v-model="codes"
                             )
-                        .sendMsg 发送验证码
+                        button.sendMsg(
+                            @click="sendCode"
+                       
+                            :class="[loding?'loading':'',!phones?'loading':'']"
+                        ) 发送验证码
+                        .time {{time}}
 
 
                 .password-from(v-show="type=='passwprd'")
                     label
                         input(
                             placeholder='用户名'
+                            v-model="usernames"
                         )
                     label
                         input(
                             placeholder='密码'
+                            v-model="passwords"
+
                         )
 
-                button 登陆
+                button(@click="login") 登陆
+            p {{phones}} -- {{codes}}
+            p {{usernames}} -- {{passwords}}
+            
+            .datas
+                | 系统管理员
+                | 测试账号：lidazhao
+                | 密码：Qwe123
+                | 手机：13800138001
+                | 验证码：654321
+                | 设备管理员
+                | 测试账号：wangerxiao
+                | 密码：Qwe123
+                | 手机：13800138002
+                | 验证码：654321
+
 
 </template>
 
 <script>
 export default {
   data() {
-    return { type: "sms" };
+    return {
+      type: "sms",
+      phones: "",
+      codes: "",
+      usernames: "",
+      passwords: "",
+      time: 3,
+      loding: false
+    };
+  },
+  methods: {
+    sendCode() {
+      if (!/^\d{11}$/.test(this.phones)) {
+        console.log("手机号码有误，请重填");
+        return false;
+      } else {
+        this.loding = true;
+        let interval = setInterval(() => {
+          --this.time;
+          if (this.time <= 0) {
+            this.loding = false;
+            this.time = 3;
+            clearInterval(interval);
+          }
+        }, 1000);
+      }
+    },
+
+    login() {
+      console.log(
+        this.type,
+        this.phones,
+        this.codes,
+        this.usernames,
+        this.passwords
+      );
+
+      if (this.type == "sms") {
+        if (!this.phones || !this.codes) {
+          console.log("请填写完整参数");
+        } else {
+          this.$http
+            .post("equipment/login", {
+              type: this.type,
+              phone: this.phones,
+              code: this.codes
+            })
+            .then(res => {
+              console.log(res);
+            });
+        }
+      } else if (this.type == "passwprd") {
+        if (this.usernames || this.passwords) {
+          console.log("请填写完整参数");
+        } else {
+          this.$http
+            .post("equipment/login", {
+              type: this.type,
+              username: this.usernames,
+              password: this.passwords
+            })
+            .then(res => {
+              console.log(res);
+            });
+        }
+      }
+    }
   }
 };
 </script>
@@ -55,6 +146,11 @@ export default {
 <style lang="sass">
 .active
     border-bottom: 2px solid #0f0
+.sendMsg.loading
+    pointer-events: none
+    background: red
+.sendMsg
+    background: #0f0
 .login
     position: fixed
     top: 0
