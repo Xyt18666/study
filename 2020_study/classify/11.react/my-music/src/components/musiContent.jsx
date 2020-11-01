2078ef67c2c02acdd25cc89e9fc6ef69adee4aa4
mingzi <419473930@qq.com>
2020-11-01 20:42:01 +0800

    ok

--- 2020_study/classify/11.react/my-music/src/components/musiContent.jsx
+++ 2020_study/classify/11.react/my-music/src/components/musiContent.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 import { connect } from "react-redux";
-
-
 
 function mapStateToProps(state) {
     return {
@@ -28,47 +26,58 @@ function mapDispathToProps(dispatch) {
         },
     };
 }
-class musicContent extends React.Component{
-    constructor(props){  
-        super(props)
+class musicContent extends React.Component {
+    constructor(props) {
+        super(props);
         this.state = {
-           dqsc:0,
-           zsc:0,
-           bl:0,
-           borWidth:0,
-           butLett:0
-        }
+            dqsc: 0,
+            zsc: 0,
+            bl: 0,
+            borWidth: 0,
+            butLett: 0,
+        };
     }
-    componentDidMount(){
-        let audio  = this.props.audios.current
-       audio.oncanplay =  ()=> {
-        setInterval( ()=> {
-            this.props.mucisActive.zsc= audio.duration
-            this.props.mucisActive.dqsc= audio.currentTime
-            this.props.setMucisActive(this.props.mucisActive)
-           
-           this.setState({
-               dqsc:this.props.mucisActive.dqsc,
-               zsc:this.props.mucisActive.zsc
-           })
-           this.setState({
-               bl:this.state.dqsc/this.state.zsc
-           })
+    componentDidMount() {
+        let audio = this.props.audios.current;
+        audio.oncanplay = () => {
+            console.log(audio.duration)
+            this.props.mucisActive.zsc = audio.duration;
+            this.props.mucisActive.dqsc = audio.currentTime;
+            this.props.setMucisActive(this.props.mucisActive);
+
             this.setState({
-                borWidth:this.state.bl * 100 +"%",
-                butLett:this.state.bl * this.proCon.current.offsetWidth
-           })
+                dqsc: this.props.mucisActive.dqsc,
+                zsc: this.props.mucisActive.zsc,
+            });
+            this.setState({
+                bl: this.state.dqsc / this.state.zsc,
+            });
+            this.setState({
+                borWidth: this.state.bl * 100 + "%",
+                butLett: this.state.bl * this.proCon.current.offsetWidth||0,
+            });
+            audio.play();
+        };
+        audio.ontimeupdate =() =>{
+            this.props.mucisActive.zsc = audio.duration;
+            this.props.mucisActive.dqsc = audio.currentTime;
+            this.props.setMucisActive(this.props.mucisActive);
 
-          
-         
-
-           
-          
-        }, 1000); //这个定时器为了输出总时长
-      };
+            this.setState({
+                dqsc: this.props.mucisActive.dqsc,
+                zsc: this.props.mucisActive.zsc,
+            });
+            this.setState({
+                bl: this.state.dqsc / this.state.zsc,
+            });
+            this.setState({
+                borWidth: this.state.bl * 100 + "%",
+                butLett: this.state.bl * this.proCon.current.offsetWidth||0,
+            });
+        };
     }
-    render(){
-         let {
+    render() {
+        let {
             // songList,
             // copySongList,
             mucisActive,
@@ -77,85 +86,109 @@ class musicContent extends React.Component{
             // setMucisActive,
         } = this.props;
 
-         this.proCon = React.createRef();
-         this.proBor = React.createRef();
+        this.proCon = React.createRef();
+        this.proBor = React.createRef();
 
-        return(
-            <section style={{
-                width:"100vw"
-            }}>
-            <div>singConten</div>
-            <p>{mucisActive.song}</p>
-            <p>{mucisActive.singer}</p>
-            <p onClick={this.toggleMusic}>开关</p>
-            <p>时间{this.getSML(this.state.dqsc)}----{this.getSML(this.props.mucisActive.zsc)}</p>
-            <div className="progress-bar-contenter" 
-            ref={this.proCon}
-            style={{
-                width:"80vw",
-                height:"10px",
-                background:"#ccc",
-                position: "relative",
-                margin:"50px auto"
-            }}>
-                <div className="progress-bar" 
-                ref={this.proBor}
+        return (
+            <section
                 style={{
-                    width:this.state.borWidth,
-                    height:"10px",
-                    background:"#0f0"
-                }}></div>
-                <div className="progress-buto" style={{
-                    position: "absolute",
-                    width:"20px",
-                    height:"20px",
-                    borderRadius:"50%",
-                    background:"red",
-                    top:"0",
-                    left:this.state.butLett,
-                    marginLeft:"-5px",
-                    marginTop:"-5px",
-                    touchAction: "none"
+                    width: "100vw",
                 }}
-                onTouchStart={this.start}
-                onTouchMove={this.move}
-
-                onTouchEnd={this.end}
-
-                
-                ></div>
-            </div>
-      
+            >
+                <div>singConten</div>
+                <p>{mucisActive.song}</p>
+                <p>{mucisActive.singer}</p>
+                <p onClick={this.toggleMusic}>开关</p>
+                <p>
+                    时间{this.getSML(this.state.dqsc)}----{this.getSML(this.props.mucisActive.zsc)}
+                </p>
+                <div
+                    className="progress-bar-contenter"
+                    ref={this.proCon}
+                    style={{
+                        width: "80vw",
+                        height: "10px",
+                        background: "#ccc",
+                        position: "relative",
+                        margin: "50px auto",
+                    }}
+                >
+                    <div
+                        className="progress-bar"
+                        ref={this.proBor}
+                        style={{
+                            width: this.state.borWidth,
+                            height: "10px",
+                            background: "#0f0",
+                        }}
+                    ></div>
+                    <div
+                        className="progress-buto"
+                        style={{
+                            position: "absolute",
+                            width: "20px",
+                            height: "20px",
+                            borderRadius: "50%",
+                            background: "red",
+                            top: "0",
+                            left: this.state.butLett,
+                            marginLeft: "-5px",
+                            marginTop: "-5px",
+                            touchAction: "none",
+                        }}
+                        onTouchStart={this.start}
+                        onTouchMove={this.move}
+                        onTouchEnd={this.end}
+                    ></div>
+                </div>
             </section>
-        )
+        );
     }
-//    start=(e)=> {
-       
-//     }
-//    move=(e) =>{
-       
-//     }
-//    end=()=> {
-   
-//     }
-    toggleMusic=()=>{
-        this.props.mucisActive.playing=!this.props.mucisActive.playing
+    start = e => {
+        let audio = this.props.audios.current;
+        audio.pause();
 
-        this.props.setMucisActive(this.props.mucisActive)
-        let audio  = this.props.audios.current
+    };
+    move = e => {
+        let bl =
+            ((e.touches[0].clientX - this.proCon.current.offsetLeft) /
+                this.proCon.current.offsetWidth) *
+            100;
+        bl = bl <= 0 ? 0 : bl >= 100 ? 100 : bl;
+        this.setState({
+            bl: bl / 100,
+        });
+
+        this.setState({
+            borWidth: this.state.bl * 100 + "%",
+            butLett: this.state.bl * this.proCon.current.offsetWidth,
+        });
+        this.setState({
+            dqsc: this.state.bl * this.state.zsc,
+        });
+    };
+    end = () => {
+        let audio = this.props.audios.current;
+        audio.currentTime = this.state.dqsc;
+        audio.play();
+    };
+    toggleMusic = () => {
+        this.props.mucisActive.playing = !this.props.mucisActive.playing;
+
+        this.props.setMucisActive(this.props.mucisActive);
+        let audio = this.props.audios.current;
         this.props.mucisActive.playing ? audio.play() : audio.pause();
-    }
-     getSML=(data)=> {
-            //时间转换 函数
-            var s = Math.floor(data / 3600);
-            var f = Math.floor((data % 3600) / 60);
-            var m = Math.floor(data % 60);
-            s = s < 10 ? "0" + s : s;
-            f = f < 10 ? "0" + f : f;
-            m = m < 10 ? "0" + m : m;
-            return s + ":" + f + ":" + m;
-        }
-  
+    };
+    getSML = data => {
+        //时间转换 函数
+        var s = Math.floor(data / 3600);
+        var f = Math.floor((data % 3600) / 60);
+        var m = Math.floor(data % 60);
+        s = s < 10 ? "0" + s : s;
+        f = f < 10 ? "0" + f : f;
+        m = m < 10 ? "0" + m : m;
+        return s + ":" + f + ":" + m;
+    };
 }
 
 const NewmusicContent = connect(mapStateToProps, mapDispathToProps)(musicContent);
