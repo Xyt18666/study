3e261336b00b6e8d8ff68eeaec24f6e0b1c327bd
Xyt18666 <419473930@qq.com>
2020-12-16 19:32:06 +0800

    f

--- 2020_study/classify/18.vue3/test-vue3/src/App3.vue
+++ 2020_study/classify/18.vue3/test-vue3/src/App3.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, onBeforeMount, onRenderTracked, onRenderTriggered } from "vue";
 import useAddStudent from "./js/add";
 import useRemoveStudent from "./js/remove";
 // 抽离方法后，引入
@@ -27,6 +27,18 @@ export default {
         let { state2, addStu } = useAddStudent(state);
         // 实例方法功能
 
+        console.log("执行了setup");
+        onBeforeMount(() => {
+            console.log("执行了onBeforeMount");
+        });
+
+        // onRenderTracked(e => {
+        //     console.log("onRenderTracked", e);
+        // });
+        // onRenderTriggered(e => {
+        //     console.log("onRenderTriggered", e);
+        // });
+
         return {
             //导出属性和方法
             state,
@@ -34,6 +46,12 @@ export default {
             state2,
             addStu,
         };
+    },
+    beforeCreate() {
+        console.log("beforeCreate");
+    },
+    created() {
+        console.log("created");
     },
 };
 </script>
