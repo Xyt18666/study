1c625020bcc0b6e2a9be64e1d79ce876d2a1f639
Xyt18666 <419473930@qq.com>
2020-12-29 10:40:32 +0800

    s

--- 2020_study/classify/7.vue/limited-music/src/main.js
+++ 2020_study/classify/7.vue/limited-music/src/main.js
@@ -22,7 +22,20 @@ Vue.component("confirm-box", ConfirmBox);
 Vue.component("switch-box", SwitchBox);
 Vue.component("base-layout", BaseLayout);
 
+Vue.mixin({
+    created: function() {
+        var myOption = this.$options.myOption;
+        if (myOption) {
+            console.log(myOption);
+        }
+    },
+    mounted() {
+        console.log(this.$options.myOption);
+    },
+});
+
 new Vue({
+    myOption: "hello Word",
     router,
     store,
     render: h => h(App),
