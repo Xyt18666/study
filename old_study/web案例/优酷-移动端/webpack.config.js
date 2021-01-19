e5a01a725e40b7c0a348cd16a8d507e4defad4b9
Xyt18666 <419473930@qq.com>
2021-01-19 21:23:19 +0800

    react

--- old_study/web案例/优酷-移动端/webpack.config.js
+++ old_study/web案例/优酷-移动端/webpack.config.js
@@ -1,30 +1,30 @@
 // 这个配置文件，其实就是一个js文件，通过node 中的模块 ，向外暴露一个配置对象
-const path = require('path')
-const htmlWebpackPluqin = require('html-webpack-plugin')
-				//只要是插件，就都放到pluguns中
-				//当我们使用了 这个插件，就不在需要手动书写script标签
-
-
+const path = require("path");
+const htmlWebpackPluqin = require("html-webpack-plugin");
+//只要是插件，就都放到pluguns中
+//当我们使用了 这个插件，就不在需要手动书写script标签
 
 module.exports = {
-	entry:path.join(__dirname,'./src/js/main.js'),//入口
-	output:{
-		path:path.join(__dirname,'./dist'),//出口
-		filename:'bundle.js'
-	},
-	plugins:[
-		new htmlWebpackPluqin({//创建一个 在内存中 生成HTML 页面的插件
-			template:path.join(__dirname,'./src/index.html'),
-			filename:'index.html'
-		})
-	],
-	module: {//用于配置所有第三方模块 加载器
-		rules: [
-			{ test:/\.css$/, use: ['style-loader','css-loader'] },
-			// {
+    entry: path.join(__dirname, "./src/js/main.js"), //入口
+    output: {
+        path: path.join(__dirname, "./dist"), //出口
+        filename: "bundle.js",
+    },
+    plugins: [
+        new htmlWebpackPluqin({
+            //创建一个 在内存中 生成HTML 页面的插件
+            template: path.join(__dirname, "./src/index.html"),
+            filename: "index.html",
+        }),
+    ],
+    module: {
+        //用于配置所有第三方模块 加载器
+        rules: [
+            { test: /\.css$/, use: ["style-loader", "css-loader"] },
+            // {
             //     test: /\.css$/,
             //     use: [
-            //         {loader: "style-loader"}, 
+            //         {loader: "style-loader"},
             //         {loader: "css-loader",
             //          options: {
             //             modules: {
@@ -32,56 +32,54 @@ module.exports = {
             //             }
             //          }
             //         }],   //打包处理css样式表的第三方loader ,启用模块化
-			// },
-	
-			{
+            // },
+
+            {
                 test: /\.less$/,
                 use: [
-					{loader: "style-loader"}, 
-                    {loader: "css-loader",
-                     options: {
-                        modules: {
-                            localIdentName: "[path][name]-[local]-[hash:5]"
-                        }
-                     }
-					},
-					{loader: "less-loader"},
-				]  //打包处理css样式表的第三方loader ,启用模块化
-			},
-			
-					
-			{ test:/\.(jpg|png|bmp|jpeg)$/, use: 'url-loader?limit=2000&name=[hash:8]-[name].[ext]'},
-									//limit给定的是图片大小，单位是字节byte，如果图片大于limit则不会被转为base64，小于则反之
-									//name是 图片命名 哈希值 原名.原后缀
-            { test:/\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader'},
-            
-			{test:  /\.jsx?$/, use:'babel-loader',exclude:/node_modules/},
-                                            //排除其他的包，只转换自己的
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                localIdentName: "[path][name]-[local]-[hash:5]",
+                            },
+                        },
+                    },
+                    { loader: "less-loader" },
+                ], //打包处理css样式表的第三方loader ,启用模块化
+            },
 
+            {
+                test: /\.(jpg|png|bmp|jpeg)$/,
+                use: "url-loader?limit=2000&name=[hash:8]-[name].[ext]",
+            },
+            //limit给定的是图片大小，单位是字节byte，如果图片大于limit则不会被转为base64，小于则反之
+            //name是 图片命名 哈希值 原名.原后缀
+            { test: /\.(ttf|eot|svg|woff|woff2)$/, use: "url-loader" },
 
-            
-		]
-	},
-	resolve:{//路径的缩写配置.这里还有问题，未实现
-		alias:{
-			Image:path.resolve(__dirname,'../images')
-		}
-	}
-}
+            { test: /\.jsx?$/, use: "babel-loader", exclude: /node_modules/ },
+            //排除其他的包，只转换自己的
+        ],
+    },
+    resolve: {
+        //路径的缩写配置.这里还有问题，未实现
+        alias: {
+            Image: path.resolve(__dirname, "../images"),
+        },
+    },
+};
 
 //配置文件
 //通过书写配置文件
-//使用 webpack 命令 
-//更新修改后的代码 
+//使用 webpack 命令
+//更新修改后的代码
 
 //当我们输入 webpack 命令后执行了
 //1.首先，webpack 发现，我们并没有通过命令的形式给他指定入口和出口
 //2.webpack 就会去 项目的 根目录 中，查找一个叫 webpack.config.js 的配置文件
 //3.当找到配置文件后，解析执行这个配置文件，当解析执行完，就得到了配置文件中的配置对象
 //4.当webpack拿到配置对象后，就拿到了 入口和出口，然后 进行打包构建
-
-
-
 
 /*
  nodemon 自动执行 node
