0a8e719b9528d81fb1f44b68c51fcd1598453a39
Xyt18666 <419473930@qq.com>
2021-03-08 16:17:22 +0800

    ok

--- 2021_study/classify/5.webpack5/模拟vue项目/webpack.config.js
+++ 2021_study/classify/5.webpack5/模拟vue项目/webpack.config.js
@@ -21,98 +21,103 @@ module.exports = {
                 loader: "vue-loader",
             },
             {
-                test: /\.css$/, //匹配哪些文件
                 oneOf: [
-                    // 这里匹配 `<style module>`
                     {
-                        resourceQuery: /module/,
-                        use: [
-                            "vue-style-loader",
+                        test: /\.css$/, //匹配哪些文件
+                        oneOf: [
+                            // 这里匹配 `<style module>`
                             {
-                                loader: "css-loader",
-                                options: {
-                                    modules: true, //开启CSS Modules
-                                    localIdentName: "[local]_[hash:base64:5]", //自定义生成的类名
-                                },
+                                resourceQuery: /module/,
+                                use: [
+                                    "vue-style-loader",
+                                    {
+                                        loader: "css-loader",
+                                        options: {
+                                            modules: true, //开启CSS Modules
+                                            localIdentName: "[local]_[hash:base64:5]", //自定义生成的类名
+                                        },
+                                    },
+                                    "postcss-loader",
+                                ],
                             },
-                            "postcss-loader",
+                            // 这里匹配普通的 `<style>` 或 `<style scoped>`
+                            {
+                                use: [
+                                    "vue-style-loader",
+                                    MiniCssExtractPlugin.loader,
+
+                                    "css-loader",
+                                    "postcss-loader",
+                                ], // 使用哪些 loader 处理
+                                //style-loader：创建一个style标签 ，将js中的样式资源插入进去，添加到 head 中
+                                //css-loader: 将css以字符串模式，变为 commonjs 模块 加载到js中，里面内容是样式字符串
+                                //ps：执行顺序为 从 后向前 依次 执行
+                                // 只有一个时 loader:""
+                            },
                         ],
                     },
-                    // 这里匹配普通的 `<style>` 或 `<style scoped>`
+
                     {
+                        test: /\.scss$/,
                         use: [
-                            "vue-style-loader",
                             MiniCssExtractPlugin.loader,
-
+                            "vue-style-loader",
                             "css-loader",
+                            "sass-loader",
                             "postcss-loader",
-                        ], // 使用哪些 loader 处理
-                        //style-loader：创建一个style标签 ，将js中的样式资源插入进去，添加到 head 中
-                        //css-loader: 将css以字符串模式，变为 commonjs 模块 加载到js中，里面内容是样式字符串
-                        //ps：执行顺序为 从 后向前 依次 执行
-                        // 只有一个时 loader:""
+                        ],
+                        // 生产环境配置 MiniCssExtractPlugin.loader   代替 style-loader  因为提取成单独文件了
+                    },
+                    {
+                        test: /\.(jpg|png|gif|)$/, //处理 img loader  , 处理不了 html 中的img
+                        loader: "url-loader",
+                        options: {
+                            publicPath: "../images/", //前缀
+                            outputPath: "images/", //输出路径
+
+                            //图片大小小于 8kb , 就会被解析为 base64 字符串 ，进行处理
+                            //优势：减少请求次数，减轻服务器压力
+                            //缺点：图片体积大，请求速度慢
+                            limit: 8 * 1024, //限制大小
+                            //问题：url-loader 使用es6模块化解析，而html-loader,引入图片是 commonjs,所以解析时会报错
+                            //解决：关闭 es6
+                            esModule: false,
+                            name: "[name][hash:8].[ext]",
+                            // 取图片 hash 的前8位    .    取文件原来扩展名
+                        },
+                    },
+                    {
+                        test: /\.html$/, //处理html 中 img ，从而能显示
+                        loader: "html-loader",
+                        // enforce: "post",
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        loader: "babel-loader", //兼容js
+                        options: {
+                            cacheDirectory: true,
+                            presets: [
+                                [
+                                    "@babel/preset-env",
+                                    {
+                                        useBuiltIns: "usage", //按需加载
+                                        corejs: {
+                                            //指定版本
+                                            version: 3,
+                                        },
+                                        targets: {
+                                            chrome: "60",
+                                            firefox: "60",
+                                            ie: "9",
+                                            safari: "10",
+                                        }, //指定兼容器做到那个版本
+                                    },
+                                ],
+                            ],
+                        },
                     },
                 ],
-            },
-
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "vue-style-loader",
-                    "css-loader",
-                    "sass-loader",
-                    "postcss-loader",
-                ],
-                // 生产环境配置 MiniCssExtractPlugin.loader   代替 style-loader  因为提取成单独文件了
-            },
-            {
-                test: /\.(jpg|png|gif|)$/, //处理 img loader  , 处理不了 html 中的img
-                loader: "url-loader",
-                options: {
-                    publicPath: "../images/", //前缀
-                    outputPath: "images/", //输出路径
-
-                    //图片大小小于 8kb , 就会被解析为 base64 字符串 ，进行处理
-                    //优势：减少请求次数，减轻服务器压力
-                    //缺点：图片体积大，请求速度慢
-                    limit: 8 * 1024, //限制大小
-                    //问题：url-loader 使用es6模块化解析，而html-loader,引入图片是 commonjs,所以解析时会报错
-                    //解决：关闭 es6
-                    esModule: false,
-                    name: "[name][hash:8].[ext]",
-                    // 取图片 hash 的前8位    .    取文件原来扩展名
-                },
-            },
-            {
-                test: /\.html$/, //处理html 中 img ，从而能显示
-                loader: "html-loader",
-                // enforce: "post",
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader", //兼容js
-                options: {
-                    presets: [
-                        [
-                            "@babel/preset-env",
-                            {
-                                useBuiltIns: "usage", //按需加载
-                                corejs: {
-                                    //指定版本
-                                    version: 3,
-                                },
-                                targets: {
-                                    chrome: "60",
-                                    firefox: "60",
-                                    ie: "9",
-                                    safari: "10",
-                                }, //指定兼容器做到那个版本
-                            },
-                        ],
-                    ],
-                },
             },
         ],
     }, //编译一些 不能识别的资源
